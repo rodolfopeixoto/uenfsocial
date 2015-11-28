72fce867706b071749a54cabb3bfe2ea9ecff108
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2015-11-28 01:50:29 -0200

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "carlabravo", email: "carlabravo@gmail.com", password: "carlabravo", password_confirmation: "carlabravo")
+User.create(username: "amandaolivera", email: "amandaolivera@gmail.com", password: "amandaolivera", password_confirmation: "amandaolivera")
+User.create(username: "melisacerqueira", email: "melisacerqueira@gmail.com", password: "melisacerqueira", password_confirmation: "melisacerqueira")
+User.create(username: "pedrolima", email: "pedrolima@gmail.com", password: "pedrolima", password_confirmation: "pedrolima")
+User.create(username: "gabrielapeixoto", email: "gabrielapeixoto@gmail.com", password: "gabrielapeixoto", password_confirmation: "gabrielapeixoto")
