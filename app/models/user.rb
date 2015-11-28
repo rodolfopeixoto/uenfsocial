b48f88020322ab62dfc9350055439ed5e3a128ba
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2015-11-28 02:01:22 -0200

    Added Friendship types

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,28 @@
 class User < ActiveRecord::Base 
 
-  validates_presence_of :username
-  validates_uniqueness_of :username
+	validates_presence_of :username
+	validates_uniqueness_of :username
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
 
   def request_friendship(user_2)
-       self.friendships.create(friend: user_2)
+  	self.friendships.create(friend: user_2)
   end
+
+  def active_friends
+  	self.friendships.where(state: "active").map(&:friend) + self.inverse_friendships.where(state: "active").map(&:friend)
+  end
+
+  def pending_friend_requests_to
+  	self.friendships.where(state: "pending")
+  end
+
+  def pending_friend_requests_from
+  	self.inverse_friendships.where(state: "pending")
+  end
+
 end
