3b4cd132039e1e61392f1f53cfbb6873ee2f0c53
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2015-11-29 03:15:10 -0200

    Added post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
 
  has_many :friendships, dependent: :destroy
  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+ has_many :posts, dependent: :destroy
 
 
 
