cbb7070dc502db9d7c830223c7d33649ab3a9c14
Soul Code <soulcodejr@gmail.com>
2016-01-19 04:28:23 -0200

    Modified routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
-  root "pages#home"
+
+  root :to => redirect("/users/sign_up")
 
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show, :index]
