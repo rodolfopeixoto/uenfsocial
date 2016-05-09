2f8ae08d1e20ae9610738ae24086c7ed39373a09
Rodolfo Peixoto - DELLI7INSPIRION <contato@rodolfopeixot.com.br>
2016-05-09 08:54:13 -0300

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
  
 
-  root :to => redirect("/users/sign_up")
-
   devise_for :users, controllers: {registrations: 'registrations'}
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+    root :to => redirect("/users/sign_up")
+
   resources :users, only: [:show, :index]
-  resources :friendships, only: [:create, :destroy, :accept] do
-      member do
-          put :accept
-      end
+  resources :friendships, only: [:create, :destroy, :accept] do 
+    member do 
+      put :accept
+    end
   end
   resources :posts, only: [:create, :edit, :update, :destroy]
   resources :activities, only: [:index]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
